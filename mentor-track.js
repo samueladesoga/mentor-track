f98f73d3fd361768937054a95dc2aeeb458c6236
Leumascom Serviced Ltd <leumascomservicedltd@Leumascoms-iMac.default>
2015-06-23 23:21:37 +0100

    able to add and delete Task from UI

--- mentor-track.js
+++ mentor-track.js
@@ -5,6 +5,28 @@ if (Meteor.isClient) {
 		}
 	});
 
+	Template.addTask.events({
+      'submit form': function(event){
+        event.preventDefault();
+        var todoValue = event.target.taskTitle.value;
+        Tasks.insert({
+        	title: todoValue,
+        	createdAt: new Date()
+        });
+        event.target.taskTitle.value = "" ; 
+        }
+	});
+
+	Template.taskItem.events({
+		'click .delete-task': function(event) {
+			event.preventDefault();
+			var confirm = window.confirm("Delete this task?");
+		    if(confirm){
+		        Tasks.remove({ _id: this._id });
+		    }
+		}
+	});
+
 }
 
 if (Meteor.isServer) {
